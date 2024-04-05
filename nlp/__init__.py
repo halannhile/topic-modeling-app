d224f4ee57fa2a63bd352c9c4da1286220ef2a3e
ian-bulovic <ian.bulovic@gmail.com>
2024-04-05 12:41:39 -0400

    fix error message

--- nlp/__init__.py
+++ nlp/__init__.py
@@ -2,6 +2,7 @@ from streamlit.runtime.uploaded_file_manager import UploadedFile
 
 SUPPORTED_INPUT_FORMATS = ["csv", "txt", "pdf"]
 
+
 def process_files(uploaded_files: list[UploadedFile]):
     if not uploaded_files:
         raise ValueError("No files uploaded for processing")
@@ -9,11 +10,11 @@ def process_files(uploaded_files: list[UploadedFile]):
     file_contents = {}
 
     for file in uploaded_files:
-        
-        file_extension = file.name.split('.')[-1].lower()
+
+        file_extension = file.name.split(".")[-1].lower()
         if file_extension not in SUPPORTED_INPUT_FORMATS:
-            raise ValueError(f"Unsupported file format: {file.type}")
-        
+            raise ValueError(f"Unsupported file format: {file_extension}")
+
         if file_extension == "txt":
             text = file.read().decode("utf-8")
             file_contents[file.name] = text
