e9f4b84f60bcef6537e562bb644e8aef535646b0
ctam8736 <ctam8736@gmail.com>
2024-04-05 11:45:50 -0400

    Fix file extension detection

--- nlp/__init__.py
+++ nlp/__init__.py
@@ -2,13 +2,22 @@ from streamlit.runtime.uploaded_file_manager import UploadedFile
 
 SUPPORTED_INPUT_FORMATS = ["csv", "txt", "pdf"]
 
-
 def process_files(uploaded_files: list[UploadedFile]):
     if not uploaded_files:
         raise ValueError("No files uploaded for processing")
 
-    for file in uploaded_files:
-        if file.type not in SUPPORTED_INPUT_FORMATS:
-            raise ValueError(f"Unsupported file format: {file.type}")
+    file_contents = {}
 
-        # TODO - Implement file processing
+    for file in uploaded_files:
+        
+        file_extension = file.name.split('.')[-1].lower()
+        if file_extension not in SUPPORTED_INPUT_FORMATS:
+            raise ValueError(f"Unsupported file format: {file.type}")
+        
+        if file_extension == "txt":
+            text = file.read().decode("utf-8")
+            file_contents[file.name] = text
+
+        # TODO - Implement file processing for csv and pdf
+
+    return file_contents
