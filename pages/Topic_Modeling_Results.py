aceb4a83ccddb0690508b7b02e1850b922cd189b
ctam8736 <ctam8736@gmail.com>
2024-04-18 01:42:33 -0400

    Add visualization page

--- pages/Topic_Modeling_Results.py
+++ pages/Topic_Modeling_Results.py
@@ -1,22 +1,48 @@
 import pandas as pd
 import streamlit as st
+from nlp.topic_modeling import get_pretrained_model, transform_doc_pretrained
+
 from database import init_db
+from database.models import Document
+from sqlalchemy import func
 
+def display_visualizations(model, texts):
 
-def display_documents():
-    st.title("Topic Modeling Results")
+    if not model:
+        model = get_pretrained_model()
 
-    db = init_db("sqlite:///db1.db")
-
-    # fetch all documents from the database
-    documents = db.get_all_documents()
-
-    df = pd.DataFrame(
-        documents, columns=["Filename", "Content", "Topic", "Probability"]
-    )
-
-    st.table(df)
-
+    model = get_pretrained_model()
+    topic_vis = model.visualize_topics(
+                            top_n_topics = 50,
+                            title = f"Topic Bubbles for BERTopicWikipedia")
+    topic_vis.update_layout(
+        title_font=dict(color="white"),
+        hoverlabel=dict(bgcolor="black"),
+        width = 800,
+        height = 800)
+    st.plotly_chart(topic_vis)
+    
+    # model.visualize_documents(texts)
 
 if __name__ == "__main__":
-    display_documents()
+
+    st.title("Topic Modeling Visualizations")
+
+    doc_db = init_db("sqlite:///db1.db")
+    doc_session = doc_db.Session()
+
+    #model_db = init_db("sqlite:///db1.db")
+    #model_session = model_db.Session()
+
+
+    # TODO: Model selection dropdown
+
+    # Document selection dropdown
+    batchnum_list = doc_session.query(Document.batch_number, func.count(Document.batch_number)) \
+                             .group_by(Document.batch_number).all()
+    batchnum_list = [f"Group {b[0]} ({b[1]} documents)" for b in batchnum_list]
+    selected_batch = st.selectbox("Documents to visualize:", batchnum_list)
+
+    if st.button("Visualize documents!", key="visualize_docs_btn"):
+        document_list = doc_session.query(Document).filter(Document.batch_number == selected_batch).all()
+        display_visualizations(None, document_list)
